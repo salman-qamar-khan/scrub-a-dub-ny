388f8888e8656c2ffaa89da234350211411f876d
Salman Qamar Khan <work.salmanqamarkhan@gmail.com>
2019-01-18 21:16:41 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'i4183521_wp2');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'i4183521_wp2');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'J&gO@eSx8p*w14n@][@65].7');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
